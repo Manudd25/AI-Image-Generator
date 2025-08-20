e69c76d03091cdff15f27b7e150aa060d7582052
Manuela Di Dario <didariomanuela@gmail.com>
2025-08-20 17:22:57 +0200

    azure pkg and env.prod. included

--- frontend-2/vite.config.js
+++ frontend-2/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/static/apps/ai-image-colab/',
+  base: './',
   plugins: [react()],
 })
